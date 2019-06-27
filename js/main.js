448094d7ef9b8cc694657f6e4cf7f6d0558121c1
KikoPT1234 <39025735+KikoPT1234@users.noreply.github.com>
2019-06-27 12:37:42 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(() => {
   $(window).on('scroll', () => {
     const scrollTop = $(this).scrollTop()
     const scrollLeft = $(this).scrollLeft()
-    $('.BannerPicture').css({
+    $('.BannerPic').css({
       'transform': `translate(${scrollLeft / 2}px, ${scrollTop / 2}px)`
     })
   })
